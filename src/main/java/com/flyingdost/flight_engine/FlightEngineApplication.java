688ea896d8de857faaaef4a6de62e08e861e0603
shivanshsinghh-ops <shivanshsinghh125@gmail.com>
2026-05-10 16:48:55 +0530

    chore: add spring boot base files

---
+++ src/main/java/com/flyingdost/flight_engine/FlightEngineApplication.java
@@ -0,0 +1,13 @@
+package com.flyingdost.flight_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flyingdost/flight_engine/FlightEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flyingdost.flight_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
